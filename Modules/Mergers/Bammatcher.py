c6af51baa352ce1e1566e629c5fe5797295f1035
Susan Liu <siyan.liu435@duke.edu>
2019-12-19 08:18:51 +0000

    Implement BamMatcher module

---
+++ Modules/Mergers/Bammatcher.py
@@ -0,0 +1,63 @@
+from Modules import PseudoMerger, Merger
+
+# Module created using CC_module_helper.py
+class Bammatcher(PseudoMerger):
+    def __init__(self, module_id, is_docker=False):
+        super(Bammatcher, self).__init__(module_id, is_docker)
+        self.output_keys = ["bammatcher_report"]
+
+    def define_input(self):
+        self.add_argument("bammatcher",     is_resource=True, is_required=True)
+        self.add_argument("config",         is_resource=True, is_required=True)
+        self.add_argument("vcf",            is_resource=True, is_required=True)
+        self.add_argument("ref",            is_resource=True, is_required=True)
+        self.add_argument("ref_idx",        is_resource=True, is_required=True)
+        self.add_argument("ref_dict",       is_resource=True, is_required=True)
+        self.add_argument("bam",            is_required=True)
+        self.add_argument("bam_idx",        is_required=True)
+        self.add_argument("sample_id",      is_required=True)
+        self.add_argument("dp-threshold",   default_value=15)
+        self.add_argument("cache-dir",      default_value="/data")
+        self.add_argument("nr_cpus",        default_value=2)
+        self.add_argument("mem",            default_value=10)
+
+    def define_output(self):
+        sample_id = self.get_argument("sample_id")
+
+        # Generate report extension
+        ext = ".{0}.{1}.report".format(sample_id[0], sample_id[1])
+
+        # Generate output filename
+        output_filepath = self.generate_unique_file_name(extension=ext)
+
+        self.add_output("bammatcher_report", output_filepath)
+
+    def define_command(self):
+        bammatcher             = self.get_argument("bammatcher")
+        config                 = self.get_argument("config")
+        vcf                    = self.get_argument("vcf")
+        dp_threshold           = self.get_argument("dp-threshold")
+        ref                    = self.get_argument("ref")
+        cache_dir              = self.get_argument("cache-dir")
+        bams                   = self.get_argument("bam")
+        output_report          = self.get_output("bammatcher_report")
+
+        # add module
+        cmd = bammatcher
+
+        # add required non-positional arguments
+        cmd += " --bam1 {}".format(bams[0])
+        cmd += " --bam2 {}".format(bams[1])
+
+        # add additional arguments
+        cmd += " --config {}".format(config)
+        cmd += " --output {}".format(output_report)
+        cmd += " --vcf {}".format(vcf)
+        cmd += " --dp-threshold {}".format(dp_threshold)
+        cmd += " --reference {}".format(ref)
+        cmd += " --cache-dir {}".format(cache_dir)
+        cmd += " !LOG2!"
+
+        return cmd
+
+
