8545710335e2de11efca66c903e7ef674c352dcb
Razvan Panea <razvan.panea@duke.edu>
2020-01-08 23:30:57 -0500

    Fix bug when defining main CC directory

--- System/__init__.py
+++ System/__init__.py
@@ -1,5 +1,5 @@
-from .GAPipeline import GAPipeline, GAPReport
-
 # Define the main CloudConductor directory
 from os.path import dirname, abspath
 CC_MAIN_DIR = dirname(dirname(abspath(__file__)))
+
+from .GAPipeline import GAPipeline, GAPReport
