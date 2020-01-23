2492fc880dbb386fa5dfc2af7f0824adeda802bb
Razvan Panea <razvan.panea@duke.edu>
2020-01-23 11:59:26 -0500

    Modify import order for Platform classes

--- System/Platform/__init__.py
+++ System/Platform/__init__.py
@@ -1,5 +1,7 @@
-from .StorageHelper import StorageHelper
-from .DockerHelper import DockerHelper
+from .Process import Process
 
 from .CloudPlatform import CloudPlatform
 from .CloudInstance import CloudInstance
+
+from .StorageHelper import StorageHelper
+from .DockerHelper import DockerHelper
