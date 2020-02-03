6de5142607a8de7727fed72f715ce52559e81249
Tushar Dave <tushar.dave@duke.edu>
2020-02-03 14:48:51 -0500

    Modify the Bedtools Coverage submodule to produce read counts

--- Modules/Tools/Bedtools.py
+++ Modules/Tools/Bedtools.py
@@ -3,22 +3,28 @@ from Modules import Module
 class Coverage(Module):
     def __init__(self, module_id, is_docker = False):
         super(Coverage, self).__init__(module_id, is_docker)
-        self.output_keys = ["coverage_report"]
+        self.output_keys = ["coverage_report", "read_counts"]
 
     def define_input(self):
-        self.add_argument("bam",        is_required=True)
-        self.add_argument("bam_idx",    is_required=True)
-        self.add_argument("ref_idx",    is_required=True, is_resource=True)
-        self.add_argument("bed",        is_required=True, is_resource=True)
-        self.add_argument("bedtools",   is_required=True, is_resource=True)
-        self.add_argument("nr_cpus",    is_required=True, default_value=2)
-        self.add_argument("mem",        is_required=True, default_value=8)
+        self.add_argument("bam",            is_required=True)
+        self.add_argument("bam_idx",        is_required=True)
+        self.add_argument("ref_idx",        is_required=True, is_resource=True)
+        self.add_argument("bed",            is_required=True, is_resource=True)
+        self.add_argument("bedtools",       is_required=True, is_resource=True)
+        self.add_argument("count_reads",    is_required=False)
+        self.add_argument("nr_cpus",        is_required=True, default_value=2)
+        self.add_argument("mem",            is_required=True, default_value=8)
 
     def define_output(self):
 
-        # Declare coverage report output filename
-        coverage_report = self.generate_unique_file_name(".coverage.txt")
-        self.add_output("coverage_report", coverage_report)
+        if self.get_argument("count_reads"):
+            # Declare reads count output filename
+            read_counts = self.generate_unique_file_name(".read.counts.txt")
+            self.add_output("read_counts", read_counts)
+        else:
+            # Declare coverage report output filename
+            coverage_report = self.generate_unique_file_name(".coverage.txt")
+            self.add_output("coverage_report", coverage_report)
 
     def define_command(self):
         # Define command for running bedtools coverage from a platform
@@ -26,11 +32,24 @@ class Coverage(Module):
         ref_idx     = self.get_argument("ref_idx")
         bed         = self.get_argument("bed")
         bedtools    = self.get_argument("bedtools")
+        count_reads = self.get_argument("count_reads")
+
+        if count_reads:
+            # get the output file name to store coverage stats
+            read_counts = self.get_output("read_counts")
+
+            # Generating coverage command for read counts
+            cmd = "{0} coverage -counts -a {1} -b {2} -sorted -g {4} > {3} !LOG2!".format(bedtools, bed, bam,
+                                                                                          read_counts, ref_idx)
+
+            return cmd
+
 
         # get the output file name to store coverage stats
         coverage_report = self.get_output("coverage_report")
 
         # Generating coverage command
-        cmd = "{0} coverage -a {1} -b {2} -sorted -g {4} > {3} !LOG2!".format(bedtools, bed, bam, coverage_report, ref_idx)
+        cmd = "{0} coverage -a {1} -b {2} -sorted -g {4} > {3} !LOG2!".format(bedtools, bed, bam, coverage_report,
+                                                                              ref_idx)
 
         return cmd
