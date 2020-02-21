eb06e22f6e1f1e04912b7903d6de5aa7ebf62b63
Qiu <qiuosier@gmail.com>
2020-02-21 12:41:16 -0500

    Update Docker module.

--- Modules/Tools/Docker.py
+++ Modules/Tools/Docker.py
@@ -5,14 +5,15 @@ logger = logging.getLogger(__name__)
 
 
 class Docker(Module):
-    def __init__(self, module_id, is_docker=True):
-        super().__init__(module_id, is_docker)
-        outputs = self.get_argument("outputs")
+    def __init__(self, module_id, is_docker=True, module_args=None):
+        logger.debug("Module Args: %s" % module_args)
+        super().__init__(module_id, is_docker, module_args=module_args)
+        outputs = self.module_args.get("outputs", dict())
         self.output_keys = outputs.keys()
         logger.debug("Output Keys: %s" % self.output_keys)
 
     def get_input_list(self):
-        inputs = self.get_argument("inputs")
+        inputs = self.module_args.get("inputs", [])
         logger.debug("Inputs: %s" % inputs)
         if inputs and not isinstance(inputs, list):
             inputs = [inputs]
@@ -31,39 +32,29 @@ class Docker(Module):
     def replace_var(s, var_dict):
         value = str(s)
         for var, val in var_dict.items():
-            value.replace(var, val)
+            value = value.replace(var, val)
         return value
 
     def define_input(self):
         self.add_argument("inputs")
         self.add_argument("outputs")
         self.add_argument("command")
-        self.add_argument("cpus")
-        self.add_argument("memory")
-        self.add_argument("barcodes")
-        logger.debug("B %s" % self.get_argument("barcodes"))
+        self.add_argument("cpus", is_required=False)
+        self.add_argument("memory", is_required=False)
 
         for arg in self.get_input_list():
             logger.debug("Adding Input: %s" % arg)
             self.add_argument(arg, is_required=False)
 
-        cpus = self.get_argument("cpus")
-        if str(cpus).isdigit():
-            nr_cpus = int(cpus)
-        else:
-            logger.debug("Getting #CPU from %s" % cpus)
-            var = self.get_argument(cpus)
-            logger.debug("%s: %s" % (cpus, var))
-            nr_cpus = len(var) if var else 1
+        cpus = self.module_args.get("cpus", 1)
+        mem = self.module_args.get("memory", 4)
 
-        mem = self.get_argument("memory")
-
-        self.add_argument("nr_cpus", is_required=False, default_value=nr_cpus)
+        self.add_argument("nr_cpus", is_required=False, default_value=cpus)
         self.add_argument("mem", is_required=False, default_value=mem)
 
     def define_output(self):
         var_dict = self.get_var_dict()
-        outputs = self.get_argument("outputs")
+        outputs = self.module_args.get("outputs")
         for key, value in outputs.items():
             is_path = True
             if isinstance(value, list):
@@ -83,10 +74,10 @@ class Docker(Module):
     def define_command(self):
         var_dict = self.get_var_dict()
         command = self.get_argument("command")
+        logger.debug("Variables: %s" % var_dict)
         command = self.replace_var(command, var_dict)
         # Covert line break to "&&" and remove empty command
-        command = " && ".join([c for c in command.split("\n") if c])
-
+        command = " && ".join([c.strip() for c in command.split("\n") if c.strip()])
         # Group commands with {} and output logs for all commands.
         cmd = "{ %s ;} !LOG3!" % command
         return cmd
