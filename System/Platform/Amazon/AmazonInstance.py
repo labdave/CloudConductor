b91ecb1d9907e703b148fc8f1a1bacd066227bdc
Clay Parker <parkerc71@gmail.com>
2020-03-12 15:55:56 -0400

    Resolving issue with spot instances requests not cancelling.

--- System/Platform/Amazon/AmazonInstance.py
+++ System/Platform/Amazon/AmazonInstance.py
@@ -203,11 +203,8 @@ class AmazonInstance(CloudInstance):
         self.wait_process("aws_configure")
 
     def destroy_instance(self):
-        if self.is_preemptible:
-            self.__cancel_spot_instance_request()
-            self.__aws_request(self.driver.destroy_node, self.node)
-        else:
-            self.__aws_request(self.driver.destroy_node, self.node)
+        self.__cancel_spot_instance_request()
+        self.__aws_request(self.driver.destroy_node, self.node)
 
     def start_instance(self):
         instance_started = False
