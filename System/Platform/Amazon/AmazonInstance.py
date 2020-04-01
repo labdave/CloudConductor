8cb36068aaa8fd508045db93d52ec8419b9fd713
Clay Parker <parkerc71@gmail.com>
2020-04-01 12:34:02 -0400

    Changing log level for more logs around AWS limits

--- System/Platform/Amazon/AmazonInstance.py
+++ System/Platform/Amazon/AmazonInstance.py
@@ -405,10 +405,10 @@ class AmazonInstance(CloudInstance):
         logging.debug(f"({self.name}) Exception is of type {e.__class__.__name__}")
         logging.debug(f"({self.name}) Print out of exception {exception_string}")
         if 'MaxSpotInstanceCountExceeded' in exception_string or 'InsufficientInstanceCapacity' in exception_string or 'InstanceLimitExceeded' in exception_string:
-            logging.error(f"({self.name}) Maximum number of spot instances exceeded.")
+            logging.info(f"({self.name}) Maximum number of spot instances exceeded.")
             return False
         if 'RequestLimitExceeded' in exception_string or 'Rate limit exceeded' in exception_string or 'ThrottlingException' in exception_string:
-            logging.error(f"({self.name}) Rate Limit Exceeded during request {method.__name__}. Sleeping for {10*count} seconds before retrying.")
+            logging.debug(f"({self.name}) Rate Limit Exceeded during request {method.__name__}. Sleeping for {10*count} seconds before retrying.")
             time.sleep(10*count)
             return True
         return False
