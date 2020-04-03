a0e3f8552772780948b6f9dab5e6a2c37c1d89c6
tushardave26@gmail.com <tushardave26@gmail.com>
2020-04-03 14:33:14 -0400

    Change the logic to rename the VC files in Jacquard

--- Modules/Mergers/Jacquard.py
+++ Modules/Mergers/Jacquard.py
@@ -41,13 +41,13 @@ class Jacquard(Merger):
         cmds = []
 
         # change the name of the VCF files based on the VC
-        for v in self.get_argument("vcf"):
-            if "haplotypecaller" in v:
-                cmds.append("mv {1} {0}/HC.vcf".format(input_dir,v))
-            elif "deepvariant" in v:
-                cmds.append("mv {1} {0}/DV.vcf".format(input_dir, v))
-            elif "variants" in v:
-                cmds.append("mv {1} {0}/STRELKA2.vcf".format(input_dir, v))
+        for sample, vcf in zip(sample_id, in_vcf):
+            if "haplotypecaller" in vcf:
+                cmds.append("mv {1} {0}/{2}_HC.vcf".format(input_dir,vcf,sample))
+            elif "deepvariant" in vcf:
+                cmds.append("mv {1} {0}/{2}_DV.vcf".format(input_dir,vcf,sample))
+            elif "variants" in vcf:
+                cmds.append("mv {1} {0}/{2}_STRELKA2.vcf".format(input_dir,vcf,sample))
 
         # add jacquard command line to list of commands
         cmds.append("{0} merge --include_format_tags=GT,AD,DP,DPI --include_rows=all --include_cells=all {1}/ {2}".format(jacquard, input_dir, output_file))
