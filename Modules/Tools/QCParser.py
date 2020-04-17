1b28802e97f23a57d8caed98dcac0d7f0164d545
tushardave26@gmail.com <tushardave26@gmail.com>
2020-04-17 15:40:21 -0400

    Add submodule to count reads from given file with read names in QCParser module

--- Modules/Tools/QCParser.py
+++ Modules/Tools/QCParser.py
@@ -710,3 +710,36 @@ class ERCCReadCounts(_QCParser):
         # Output qc_report to file
         cmd += " > %s !LOG2!" % qc_report
         return cmd
+
+
+class ReadNameCounts(_QCParser):
+
+    def __init__(self, module_id, is_docker=False):
+        super(ReadNameCounts, self).__init__(module_id, is_docker)
+
+    def define_input(self):
+        super(ReadNameCounts, self).define_input()
+        self.add_argument("read_names", is_required=True)
+
+        # Overwrite the default values for nr_cpus and mem
+        self.add_argument("nr_cpus", is_required=True, default_value=2)
+        self.add_argument("mem", is_required=True, default_value=4)
+
+    def define_command(self):
+        # Get options from kwargs
+        input_file      = self.get_argument("read_names")
+        qc_parser       = self.get_argument("qc_parser")
+        sample_name     = self.get_argument("sample_name")
+        parser_note     = self.get_argument("note")
+        qc_report       = self.get_output("qc_report")
+
+        # Generate base command
+        cmd = "%s ReadNameCounts -i %s -s %s" % (qc_parser, input_file, sample_name)
+
+        # Add parser note if necessary
+        if parser_note is not None:
+            cmd += " -n \"%s\"" % parser_note
+
+        # Output qc_report to file
+        cmd += " > %s !LOG2!" % qc_report
+        return cmd
