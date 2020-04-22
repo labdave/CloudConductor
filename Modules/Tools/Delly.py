87e11ba93d94d835386e5ab442661e4f71d4c5f5
Devang <>
2020-04-22 16:35:59 -0400

    add bcftools as resource

--- Modules/Tools/Delly.py
+++ Modules/Tools/Delly.py
@@ -9,6 +9,7 @@ class Delly(Module):
     def define_input(self):
         self.add_argument("bam",            is_required=True)
         self.add_argument("delly",          is_required=True, is_resource=True)
+        self.add_argument("bcftools",       is_required=True, is_resource=True)
         self.add_argument("ref",            is_required=True, is_resource=True)
         self.add_argument("exclude_list",   is_resource=True)
         self.add_argument("nr_cpus",        is_required=True, default_value=2)
@@ -29,6 +30,7 @@ class Delly(Module):
         ref             = self.get_argument("ref")
         exclude_list    = self.get_argument("exclude_list")
         delly           = self.get_argument("delly")
+        bcftools        = self.get_argument("bcftools")
 
         # Get output paths
         vcf             = self.get_output("delly_vcf")
@@ -42,6 +44,6 @@ class Delly(Module):
         else:
             cmd = "{0} call -g {1} -o {2} {3};".format(delly, ref, bcf, bam)
 
-        cmd += "bcftools view {0} > {1} !LOG3!".format(bcf, vcf)
+        cmd += "{0} view {1} > {2} !LOG3!".format(bcftools, bcf, vcf)
 
         return cmd
