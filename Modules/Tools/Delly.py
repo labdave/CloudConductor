3ca209d7587a2a6db1461b811a04e2eb9ab16f2d
Devang <>
2020-04-22 18:07:53 -0400

    debug bcftools situation

--- Modules/Tools/Delly.py
+++ Modules/Tools/Delly.py
@@ -37,11 +37,12 @@ class Delly(Module):
         bcf             = self.generate_unique_file_name("delly.bcf")
 
         # Generate command
-        if exclude_list:
-            cmd = "{0} call -x {1} -g {2} -o {3} {4};".format(delly, exclude_list, ref, bcf, bam)
-        else:
-            cmd = "{0} call -g {1} -o {2} {3};".format(delly, ref, bcf, bam)
-        print(delly)
-        cmd += "/bcftools/bcftools view {0} > {1} !LOG3!".format(bcf, vcf)
+        cmd = 'ls -l'
+        # if exclude_list:
+        #     cmd = "{0} call -x {1} -g {2} -o {3} {4};".format(delly, exclude_list, ref, bcf, bam)
+        # else:
+        #     cmd = "{0} call -g {1} -o {2} {3};".format(delly, ref, bcf, bam)
+        # print(delly)
+        # cmd += "/bcftools/bcftools view {0} > {1} !LOG3!".format(bcf, vcf)
 
         return cmd
