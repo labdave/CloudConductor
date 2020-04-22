25299eb9a24b3fadc8a5526e845485f5b224f0ac
Devang <>
2020-04-22 17:28:53 -0400

    debug bcftools situation

--- Modules/Tools/Delly.py
+++ Modules/Tools/Delly.py
@@ -41,7 +41,7 @@ class Delly(Module):
             cmd = "{0} call -x {1} -g {2} -o {3} {4};".format(delly, exclude_list, ref, bcf, bam)
         else:
             cmd = "{0} call -g {1} -o {2} {3};".format(delly, ref, bcf, bam)
-
-        cmd += "bcftools view {0} > {1} !LOG3!".format(bcf, vcf)
+        print(delly)
+        cmd += "/usr/bin/bcftools view {0} > {1} !LOG3!".format(bcf, vcf)
 
         return cmd
