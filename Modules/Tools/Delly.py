40f39e442a82df2ee27a241ec4e55d8d90a030b4
Devang <>
2020-04-23 23:09:40 -0400

    debugging

--- Modules/Tools/Delly.py
+++ Modules/Tools/Delly.py
@@ -38,11 +38,10 @@ class Delly(Module):
         bcf             = "/data/delly/output/temp.bcf"
 
         # Generate command
-        # cmd = 'touch {0}; touch {1}; ls -l /usr/bin/ !LOG3!'.format(vcf, bcf)
         if exclude_list:
             cmd = "{0} call -x {1} -g {2} -o {3} {4} !LOG3!".format(delly, exclude_list, ref, bcf, bam)
         else:
             cmd = "{0} call -g {1} -o {2} {3} !LOG3!".format(delly, ref, bcf, bam)
-        # cmd += ";/usr/bin/bcftools view {0} > {1} !LOG3!".format(bcf, vcf)
+        cmd += ";/usr/bin/bcftools view {0} > {1} !LOG3!".format(bcf, vcf)
 
         return cmd
