c9b2337229748353ec478e46d381e0b402ba65e8
Clay Parker <parkerc71@gmail.com>
2020-04-27 12:45:23 -0400

    Added logging around disk size calculations

--- System/Graph/TaskWorker.py
+++ System/Graph/TaskWorker.py
@@ -322,6 +322,7 @@ class TaskWorker(Thread):
 
         # Set size of desired disk
         disk_size = int(math.ceil(input_multiplier * input_size))
+        input_size = disk_size
 
         # Make sure platform can create a disk that size
         min_disk_size = self.platform.get_min_disk_space()
@@ -332,6 +333,8 @@ class TaskWorker(Thread):
 
         # Must be at least as big as minimum disk size + disk image
         disk_size = disk_size + disk_image_size + min_disk_size
+
+        logging.debug(f"({self.task.get_ID()}) Calculated Input size: {disk_size}, Disk Image Size: {disk_image_size}, Added Disk Size: {min_disk_size}")
 
         # And smaller than max disk size
         if disk_size > max_disk_size:
