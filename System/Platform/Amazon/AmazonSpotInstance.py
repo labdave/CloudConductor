99434cddb14fb4dee97dff7a1cf6fc20706ded23
Clay Parker <parkerc71@gmail.com>
2020-04-29 21:42:53 -0400

    Fix issues with instance conversion from preempt to standard

--- System/Platform/Amazon/AmazonSpotInstance.py
+++ System/Platform/Amazon/AmazonSpotInstance.py
@@ -142,14 +142,14 @@ class AmazonSpotInstance(AmazonInstance):
                     logging.debug(f"({self.name}) Failed to stop instance. ResourceNotFound... recreating.")
 
                     # Recreate the instance
-                    self = self.recreate()
+                    self.recreate()
 
                     # Instance recreation complete
                     logging.debug("(%s) Instance recreated, rerunning all processes!" % self.name)
 
         else:
             # Recreate the instance
-            self = self.recreate()
+            self.recreate()
 
             # Instance recreation complete
             logging.debug("(%s) Instance recreated, rerunning all processes!" % self.name)
