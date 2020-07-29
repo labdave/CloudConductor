ee85a59a107d39edbb4b40ecd55133bf106d1334
Devang <devang01121995@gmail.com>
2020-07-28 21:05:43 -0400

    add nonsplit readsbam

--- Modules/Tools/IGV_Translocation.py
+++ Modules/Tools/IGV_Translocation.py
@@ -15,6 +15,8 @@ class IGV_Translocation(Module):
 		self.add_argument("anno_vcf",				is_required=True)
 		self.add_argument("bam",					is_required=True)
 		self.add_argument("bam_idx",				is_required=True)
+		self.add_argument("non_split_bam",			is_required=True)
+		self.add_argument("non_split_bam_idx",		is_required=True)
 		self.add_argument("nr_cpus",				default_value=2)
 		self.add_argument("mem",					default_value=10)
 		self.add_argument("pe_sr_threshold",		default_value=10)
@@ -37,6 +39,7 @@ class IGV_Translocation(Module):
 		# Module creator needs to use renamed arguments as required by CC
 		anno_vcf					= self.get_argument("anno_vcf")
 		bam							= self.get_argument("bam")
+		non_split_bam				= self.get_argument("non_split_bam")
 		pe_sr_threshold				= self.get_argument("pe_sr_threshold")
 		split						= self.get_argument("split")
 		grouped						= self.get_argument("grouped")
@@ -52,9 +55,12 @@ class IGV_Translocation(Module):
 		cmd = "python filter_rows.py {0} {1} filtered_records.tsv !LOG3!;".format(anno_vcf, pe_sr_threshold)
 		cmd += "cat filtered_records.tsv !LOG3!;"
 		cmd += "mkdir -p {0}; !LOG3!;".format(igv_translocation_dir)
-		cmd += "mkdir -p {0}/nonsquished; !LOG3!;".format(igv_translocation_dir)
-		cmd += "mkdir -p {0}/squished; !LOG3!;".format(igv_translocation_dir)
+		cmd += "mkdir -p {0}/nonsquished/nosplitreads; !LOG3!;".format(igv_translocation_dir)
+		cmd += "mkdir -p {0}/squished/nosplitreads; !LOG3!;".format(igv_translocation_dir)
+		cmd += "mkdir -p {0}/nonsquished/splitreads; !LOG3!;".format(igv_translocation_dir)
+		cmd += "mkdir -p {0}/squished/splitreads; !LOG3!;".format(igv_translocation_dir)
 
+		# BAM WITH SPLIT READS
 
 		# run non-squished version
 		cmd += "python igv_script_creator.py -f filtered_records.tsv -o nonsquished.script -b {0} -z {1} -t {2} -r {3} -d {4}/nonsquished".format(bam, zoom, bed, repeat_blacklist, igv_translocation_dir)
@@ -72,18 +78,35 @@ class IGV_Translocation(Module):
 			cmd += " -g"
 		cmd += " -q !LOG3!;"
 
+		# BAM WITH NO SPLIT READS
+
+		# run non-squished version
+		cmd += "python igv_script_creator.py -f filtered_records.tsv -o nonsquished_nosplitreads.script -b {0} -z {1} -t {2} -r {3} -d {4}/nonsquished".format(non_split_bam, zoom, bed, repeat_blacklist, igv_translocation_dir)
+		if split:
+			cmd += " -s"
+		if grouped:
+			cmd += " -g"
+		cmd += " !LOG3!;"
+
+		# run squished version
+		cmd += "python igv_script_creator.py -f filtered_records.tsv -o squished_nosplitreads.script -b {0} -z {1} -t {2} -r {3} -d {4}/squished".format(non_split_bam, zoom, bed, repeat_blacklist, igv_translocation_dir)
+		if split:
+			cmd += " -s"
+		if grouped:
+			cmd += " -g"
+		cmd += " -q !LOG3!;"
+
 		cmd += "cat nonsquished.script !LOG3!; cat squished.script !LOG3!;"
+		cmd += "cat nonsquished_nosplitreads.script !LOG3!; cat squished_nosplitreads.script !LOG3!;"
 		
 		# run igv non-squished version
 		cmd += "xvfb-run --auto-servernum --server-args=\"-screen 0 4000x2400x24\" java -Xmx{0}000m -jar IGV_2.3.81/igv.jar -b nonsquished.script !LOG3!;".format(nr_cpus)
 		# run igv squished version
 		cmd += "xvfb-run --auto-servernum --server-args=\"-screen 0 4000x2400x24\" java -Xmx{0}000m -jar IGV_2.3.81/igv.jar -b squished.script !LOG3!".format(nr_cpus)
-		# cmd += " run /usr/local/bin/destruct_ref/ {0} {1} {2} --bam_files {3}".format(
-		# 	breaks, break_libs, break_reads, bam)
-
-		# cmd += " --lib_ids {0} --maxjobs {1} --submit {2}".format(lib_ids, nr_cpus, submit)
-
-		# # add logging
-		# cmd += " !LOG3!"
-
+		
+		# run igv non-squished version nosplitreads
+		cmd += "xvfb-run --auto-servernum --server-args=\"-screen 0 4000x2400x24\" java -Xmx{0}000m -jar IGV_2.3.81/igv.jar -b nonsquished_nosplitreads.script !LOG3!;".format(nr_cpus)
+		# run igv squished version nosplitreads
+		cmd += "xvfb-run --auto-servernum --server-args=\"-screen 0 4000x2400x24\" java -Xmx{0}000m -jar IGV_2.3.81/igv.jar -b squished_nosplitreads.script !LOG3!".format(nr_cpus)
+		
 		return cmd
