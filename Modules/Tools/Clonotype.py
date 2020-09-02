77f194bb2981e76593aa37660656c23f1604777d
Devang <devang01121995@gmail.com>
2020-09-02 18:18:44 -0400

    add all clones to clonotype

--- Modules/Tools/Clonotype.py
+++ Modules/Tools/Clonotype.py
@@ -3,51 +3,57 @@ from Modules import Module
 class Clonotype(Module):
     def __init__(self, module_id, is_docker = False):
         super(Clonotype, self).__init__(module_id, is_docker)
-        self.output_keys = ['ig_clones', 't_clones', 'ig_report', 't_report']
+        self.output_keys = ['top_ig_clones', 'all_ig_clones', 'top_t_clones', 'all_t_clones', 'ig_report', 't_report']
 
     def define_input(self):
-        self.add_argument('bam',        is_required=True)
-        self.add_argument('bam_idx',    is_required=True)
-        self.add_argument('ig',         default_value=False)
-        self.add_argument('t',          default_value=False)
-        self.add_argument('nr_cpus',    default_value=4)
-        self.add_argument('mem',        default_value=30)
-        self.add_argument('logging',    default_value='!LOG3!')
+        self.add_argument('bam',                is_required=True)
+        self.add_argument('bam_idx',            is_required=True)
+        self.add_argument('ig',                 default_value=False)
+        self.add_argument('t',                  default_value=False)
+        self.add_argument('nr_cpus',            default_value=4)
+        self.add_argument('mem',                default_value=30)
+        self.add_argument('logging',            default_value='!LOG3!')
 
     def define_output(self):
-        ig_clones   = self.generate_unique_file_name('IG.clones.tsv')
-        t_clones    = self.generate_unique_file_name('T.clones.tsv')
-        ig_report   = self.generate_unique_file_name('.IG.report')
-        t_report    = self.generate_unique_file_name('.T.report')
+        top_ig_clones       = self.generate_unique_file_name('IG.top.clones.tsv')
+        all_ig_clones       = self.generate_unique_file_name('IG.all.clones.tsv')
+        top_t_clones        = self.generate_unique_file_name('T.top.clones.tsv')
+        all_t_clones        = self.generate_unique_file_name('T.all.clones.tsv')
+        ig_report           = self.generate_unique_file_name('.IG.report')
+        t_report            = self.generate_unique_file_name('.T.report')
 
-        ig          = self.get_argument('ig')
-        t           = self.get_argument('t')
+        ig                  = self.get_argument('ig')
+        t                   = self.get_argument('t')
 
         if ig:
-            self.add_output('ig_clones', ig_clones)
-            self.add_output('ig_report', ig_report)
+            self.add_output('top_ig_clones',    top_ig_clones)
+            self.add_output('all_ig_clones',    all_ig_clones)
+            self.add_output('ig_report',        ig_report)
         if t:
-            self.add_output('t_clones', t_clones)
-            self.add_output('t_report', t_report)
+            self.add_output('top_t_clones',    top_t_clones)
+            self.add_output('all_t_clones',    all_t_clones)
+            self.add_output('t_report',        t_report)
 
     def define_command(self):
-        bam         = self.get_argument('bam')
-        threads     = self.get_argument('nr_cpus')
-        logging     = self.get_argument('logging')
-        ig          = self.get_argument('ig')
-        t           = self.get_argument('t')
+        bam                 = self.get_argument('bam')
+        threads             = self.get_argument('nr_cpus')
+        logging             = self.get_argument('logging')
+        ig                  = self.get_argument('ig')
+        t                   = self.get_argument('t')
 
-        cmd_ig      = ''
-        cmd_t       = ''
+        cmd_ig              = ''
+        cmd_t               = ''
 
         if ig:
-            ig_clones   = self.get_output('ig_clones')
-            ig_report   = self.get_output('ig_report')
-            cmd_ig  = 'bash BCR.sh {0} {1} {2} {3} {4}'.format(bam, threads, ig_report, ig_clones, logging)
+            top_ig_clones   = self.get_output('top_ig_clones')
+            all_ig_clones   = self.get_output('all_ig_clones')
+            ig_report       = self.get_output('ig_report')
+            cmd_ig = 'bash BCR.sh {0} {1} {2} {3} {4} {5}'.format(bam, threads, ig_report, top_ig_clones, all_ig_clones, logging)
         if t:
-            t_clones    = self.get_output('t_clones')
-            t_report    = self.get_output('t_report')
-            cmd_t   = 'bash TCR.sh {0} {1} {2} {3} {4}'.format(bam, threads, t_report, t_clones, logging)
+            top_t_clones    = self.get_output('top_t_clones')
+            all_t_clones    = self.get_output('all_t_clones')
+            t_report        = self.get_output('t_report')
+            cmd_t = 'bash TCR.sh {0} {1} {2} {3} {4} {5}'.format(bam, threads, t_report, top_t_clones, all_t_clones, logging)
 
         if cmd_ig and cmd_ig:
             return[cmd_ig, cmd_t]
