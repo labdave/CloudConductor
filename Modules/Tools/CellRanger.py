af844bb4eebf4643bcd4e4aa18a1e89cedc325fa
Devang <devang01121995@gmail.com>
2020-09-18 19:11:45 -0400

    fix unzip issue

--- Modules/Tools/CellRanger.py
+++ Modules/Tools/CellRanger.py
@@ -78,7 +78,7 @@ class CellRanger(Module):
             mv_R2_cmd += "mv -u /data/{0} {1};".format(R2[i], new_R2)
 
         cmd = ""
-        cmd += "gunzip {0};".format(cellranger)
+        cmd += "unzip {0};".format(cellranger)
         cmd += "tar -zxvf {0};".format(transcriptome)
         cmd += "ls -l !LOG3!; ls -l /data/ !LOG3!;"
         cmd += "source {0} !LOG3!; mkdir /data/fastqs;".format(source_path)
