2416920e672deb2c34e4fb9173a9fb8c12b712ce
Devang <devang01121995@gmail.com>
2020-09-18 02:55:19 -0400

    debug

--- Modules/Tools/CellRanger.py
+++ Modules/Tools/CellRanger.py
@@ -62,15 +62,17 @@ class CellRanger(Module):
             # TEMPORARY HARDCODING
             lane_R1 = R1[i].split("-")[-2][-1]
             lane_R2 = R2[i].split("-")[-2][-1]
+            samp_R1 = R1[i].split("_")[1][-1]
+            samp_R2 = R2[i].split("_")[1][-1]
 
-            new_R1 = "/data/fastqs/sample_S1_L00{0}_R1_001.fastq.gz".format(lane_R1)
-            new_R2 = "/data/fastqs/sample_S1_L00{0}_R2_001.fastq.gz".format(lane_R2)
+            new_R1 = "/data/fastqs/sample_S{0}_L00{1}_R1_001.fastq.gz".format(samp_R1, lane_R1)
+            new_R2 = "/data/fastqs/sample_S{0}_L00{1}_R2_001.fastq.gz".format(samp_R2, lane_R2)
             mv_R1_cmd += "mv -u /data/{0} {1};".format(R1[i], new_R1)
             mv_R2_cmd += "mv -u /data/{0} {1};".format(R2[i], new_R2)
 
         cmd = "source {0} !LOG3!; mkdir /data/fastqs; {1}{2} ls /data/fastqs/ !LOG3!;" \
-              "cellranger-4.0.0/cellranger count --id={3} --fastqs=/data/fastqs/" \
-              "--transcriptome={4} --localcores={5} --localmem={6} !LOG3! ".format(
+              "cellranger-4.0.0/cellranger count --id {3} --fastqs /data/fastqs/" \
+              "--transcriptome {4} --localcores {5} --localmem {6} !LOG3! ".format(
             source_path, mv_R1_cmd, mv_R2_cmd, sample_name, transcriptome, nr_cpus, mem)
 
         return cmd
