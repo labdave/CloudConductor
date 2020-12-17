6d06dbf33cbd0d0939346eb6f6fe9c0df302aabb
lanieehapp <lanieehapp@gmail.com>
2020-12-17 06:47:16 -0800

    fixed typo in output type

--- Modules/Mergers/VariantMerger.py
+++ Modules/Mergers/VariantMerger.py
@@ -1,3 +1,4 @@
+import os
 from Modules import Merger
 
 class VariantMerger(Merger):
@@ -7,21 +8,21 @@ class VariantMerger(Merger):
         self.output_keys    = ["merged_filt_long", "merged_filt_wide", "merged_wl_long", "merged_wl_wide"]
 
     def define_input(self):
-        self.add_argument("nr_cpus",                        is_required=True, default_value=8)
-        self.add_argument("mem",                            is_required=True, default_value=48)
+        self.add_argument("nr_cpus",         is_required=True, default_value=8)
+        self.add_argument("mem",             is_required=True, default_value=48)
 
 
 
     def define_output(self):
         # Declare name of merged VCF output file
-        merged_filt_long = self.generate_unique_file_name(extension="merged_filtered_long.txt")
-        merged_filt_wide = self.generate_unique_file_name(extension="merged_filtered_wide.txt")
-        merged_wl_long = self.generate_unique_file_name(extension="merged_whitelist_long.txt")
-        merged_wl_wide = self.generate_unique_file_name(extension="merged_whitelist_wide.txt")
+        merged_filt_long = self.generate_unique_file_name(extension="_merged_filtered_long.txt")
+        merged_filt_wide = self.generate_unique_file_name(extension="_merged_filtered_wide.txt")
+        merged_wl_long = self.generate_unique_file_name(extension="_merged_whitelist_long.txt")
+        merged_wl_wide = self.generate_unique_file_name(extension="_merged_whitelist_wide.txt")
         self.add_output("merged_filt_long", merged_filt_long)
         self.add_output("merged_filt_wide", merged_filt_wide)
-        self.add_ouput("merged_wl_long", merged_wl_long)
-        self.add_ouput("merged_wl_wide", merged_wl_wide)
+        self.add_output("merged_wl_long", merged_wl_long)
+        self.add_output("merged_wl_wide", merged_wl_wide)
 
     def define_command(self):
         # Get input arguments
