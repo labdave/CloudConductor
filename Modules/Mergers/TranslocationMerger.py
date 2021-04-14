48518dc0783dab3ab487b21d229d7fc626700346
Rachel Kositsky <rachel.kositsky@duke.edu>
2021-04-14 16:39:33 -0400

    Add collapsing step to translocation merging
    Multiple outputs now: concatenated output and collapsed output
    Only do collapsing on filtered translocations because the raw ones would take too long

--- Modules/Mergers/TranslocationMerger.py
+++ Modules/Mergers/TranslocationMerger.py
@@ -3,27 +3,35 @@ from Modules import Merger
 class AggregateTranslocations(Merger):
     def __init__(self, module_id, is_docker = False):
         super(AggregateTranslocations, self).__init__(module_id, is_docker)
-        self.output_keys = ["merged_all_trls", "merged_filt_trls", "merged_fish_trls"]
+        self.output_keys = ["concatenated_all_trls", "concatenated_filt_trls", 
+        "concatenated_fish_trls", "collapsed_filt_trls", "collapsed_fish_trls"]
 
     def define_input(self):
         self.add_argument("all_translocations",  is_required=True)
         self.add_argument("filt_translocations", is_required=True)
         self.add_argument("fish_translocations", is_required=True)
+        self.add_argument("merge_distance",      default_value=10)
         self.add_argument("mem",                 default_value=10)
         self.add_argument("nr_cpus",             default_value=2)
 
     def define_output(self):
-        merged_all_trls     = self.generate_unique_file_name(extension=".merged.all.tsv")
-        self.add_output("merged_all_trls",  merged_all_trls)
-        merged_filt_trls     = self.generate_unique_file_name(extension=".merged.filtered.tsv")
-        self.add_output("merged_filt_trls",  merged_filt_trls)
-        merged_fish_trls      = self.generate_unique_file_name(extension=".merged.FISH.tsv")
-        self.add_output("merged_fish_trls",   merged_fish_trls)
+        concatenated_all_trls     = self.generate_unique_file_name(extension=".concat.all.tsv")
+        self.add_output("concatenated_all_trls",  concatenated_all_trls)
+        concatenated_filt_trls     = self.generate_unique_file_name(extension=".concat.filtered.tsv")
+        self.add_output("concatenated_filt_trls",  concatenated_filt_trls)
+        concatenated_fish_trls      = self.generate_unique_file_name(extension=".concat.FISH.tsv")
+        self.add_output("concatenated_fish_trls",   concatenated_fish_trls)
+        collapsed_filt_trls      = self.generate_unique_file_name(extension=".collapsed.filtered.tsv")
+        self.add_output("collapsed_filt_trls",   collapsed_filt_trls)
+        collapsed_fish_trls      = self.generate_unique_file_name(extension=".collapsed.FISH.tsv")
+        self.add_output("collapsed_fish_trls",   collapsed_fish_trls)
+        
 
     def define_command(self):
         all_translocations  = self.get_argument("all_translocations")
         filt_translocations = self.get_argument("filt_translocations")
         fish_translocations = self.get_argument("fish_translocations")
+        merge_distance      = self.get_argument("merge_distance")
 
         # Turn all inputs into lists if they're not already for the later .join
         if not isinstance(all_translocations, list):
@@ -33,14 +41,19 @@ class AggregateTranslocations(Merger):
         if not isinstance(fish_translocations, list):
             fish_translocations = [fish_translocations]
 
-        merged_all_trls  = self.get_output("merged_all_trls")
-        merged_filt_trls = self.get_output("merged_filt_trls")
-        merged_fish_trls = self.get_output("merged_fish_trls")
+        concatenated_all_trls  = self.get_output("concatenated_all_trls")
+        concatenated_filt_trls = self.get_output("concatenated_filt_trls")
+        concatenated_fish_trls = self.get_output("concatenated_fish_trls")
+
+        collapsed_filt_trls = self.get_output("collapsed_filt_trls")
+        collapsed_fish_trls = self.get_output("collapsed_fish_trls")
 
         cmd = ""
-        cmd += f"python merge_translocations.py {merged_all_trls} {' '.join(all_translocations)} !LOG3!; "
-        cmd += f"python merge_translocations.py {merged_filt_trls} {' '.join(filt_translocations)} !LOG3!; "
-        cmd += f"python merge_translocations.py {merged_fish_trls} {' '.join(fish_translocations)} !LOG3!; "
+        cmd += f"python concatenate_translocations.py {concatenated_all_trls} {' '.join(all_translocations)} !LOG3!; "
+        cmd += f"python concatenate_translocations.py {concatenated_filt_trls} {' '.join(filt_translocations)} !LOG3!; "
+        cmd += f"python concatenate_translocations.py {concatenated_fish_trls} {' '.join(fish_translocations)} !LOG3!; "
+        cmd += f"Rscript collapse_translocations.R -o {collapsed_filt_trls} -i {','.join(filt_translocations)} -d {merge_distance} !LOG3!; "
+        cmd += f"Rscript collapse_translocations.R -o {collapsed_fish_trls} -i {','.join(fish_translocations)} -d {merge_distance} !LOG3!; "
 
         return cmd
 
